c456831adcfe9660d3fcff1f6de9f41e19df3e07
Hanna <32440084+Yasko1@users.noreply.github.com>
2019-05-29 00:27:32 +0300

    Update WordParser.java

--- Task2/by.epam.javatraining/controller/WordParser.java
+++ Task2/by.epam.javatraining/controller/WordParser.java
@@ -18,31 +18,3 @@ public class WordParser implements ParserChain<Output> {
         return wordWithSymbols;
     }
 }
-	
-	/*
-	private static final Logger log = Logger.getLogger(SentenceParser.class);
-	
-	private static String dividersForWords = "(\\W+|\\d+)"; // regular expressions
-
-	public static List<Word> getWords(List<String> linesFromFile, List<Word> words) {
-		try {
-		if (linesFromFile.isEmpty()) {
-			throw new EmptyListException("File is empty!");
-		}
-		}catch(EmptyListException e) {
-			log.info("File is empty, nothing to split");
-		}
-		for (String line : linesFromFile) {
-			if (!line.isEmpty()) {
-				String[] wordsInOneLine = line.split(dividersForWords);
-				for (String word : wordsInOneLine) {
-					if (!word.isEmpty()) {
-						words.add(new Word(word));
-					}
-				}
-			}
-		}
-		return words;
-	}
-	}*/
-
