5a177e037629f606a77e9fb2dd6350760987e7f0
Hanna <32440084+Yasko1@users.noreply.github.com>
2019-06-07 22:00:39 +0300

    Add files via upload

---
+++ Threads/demo/src/by/epam/javatraining/task15/ChannelPool.java
@@ -0,0 +1,21 @@
+package by.epam.javatraining.task15;
+
+public class ChannelPool <T> {
+    private final static int POOL_SIZE = 5;
+    // размер пула private
+    final Semaphore semaphore = new Semaphore(POOL_SIZE, true);
+    private final Queue<T> resources = new LinkedList<T>();
+    public ChannelPool(Queue<T> source) {
+        resources.addAll(source);
+    }
+    public T getResource(long maxWaitMillis) throws ResourсeException {
+        try {
+            if (semaphore.tryAcquire(maxWaitMillis, TimeUnit.MILLISECONDS)) {
+                T res = resources.poll();
+                return res;
+            }
+        } catch (InterruptedException e) {
+            throw new ResourсeException(e);
+        }
+    } throw new ResourсeException(":превышено время ожидания"); } public void returnResource(T res) { resources.add(res); // возвращение экземпляра в пул semaphore.release(); }
+}
